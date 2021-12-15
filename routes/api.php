ee8e42b2dca2afca89bbaacae23c89c8b2f3c915
tran phuoc vinh <phuocvinh08112001@gmail.com>
2021-12-15 21:42:47 +0700

    up api sanpham

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SanPhamController;
-<<<<<<< HEAD
 use App\Models\SanPham;
-=======
->>>>>>> c8e90bfc96f3238f19753168f30bae80dc84b637
 use App\Http\Controllers\KhachHangController;
 
 
@@ -25,14 +22,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get('SanPham', [SanPhamController::class,"API_SanPham"]);
-<<<<<<< HEAD
 
 # api loại sản phẩm điện thoại 
 Route::get('dien-thoai',[SanPhamController::class,"API_SanPham_DT"]);
 
 # api chi tiết sản phẩm điện thoại
 Route::get('san-pham/{id}',[SanPhamController::class,"API_SanPham_DT_ChiTiet"]);
-=======
->>>>>>> c8e90bfc96f3238f19753168f30bae80dc84b637
 Route::post('DangNhap', [KhachHangController::class,"API_DangNhap"]);
 Route::post('DangKy', [KhachHangController::class,"API_DangKy"]);
